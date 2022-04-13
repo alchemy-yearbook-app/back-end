12afcce53e93705e3b76b175ee9df98a8fa506f4
Kevin Do <kevind0501@gmail.com>
2022-04-13 11:57:23 -0700

    moved test file

--- __tests__/github.test.js
+++ __tests__/github.test.js
@@ -15,37 +15,6 @@ describe('yearbook app routes', () => {
     pool.end();
   });
 
-  it('redirects to the github oauth page upon login', async () => {
-    const req = await request(app).get('/api/v1/github/login');
-
-    expect(req.header.location).toMatch(
-      /https:\/\/github.com\/login\/oauth\/authorize\?client_id=[\w\d]+&scope=user&redirect_uri=http:\/\/localhost:7890\/api\/v1\/github\/login\/callback/i
-    );
-  });
-
-  it('should login and redirect to profile', async () => {
-    const req = await request
-      .agent(app)
-      .get('/api/v1/github/login/callback?code=42')
-      .redirects(1);
-
-    expect(req.redirects[0]).toEqual(
-      expect.stringContaining('/api/v1/profile')
-    );
-  });
-
-  it.skip('logs a user out through a delete route', async () => {
-    await GithubUser.createUser({
-      username: 'fake_github_user',
-      avatar_url: 'https://www.placecage.com/gif/300/300',
-      email: 'not-real@example.com',
-    });
-
-    const res = await request(app).delete('/api/v1/github/sessions');
-
-    expect(res.body.message).toEqual('Signed out successfully');
-  });
-
   it.only('lists teams in the organization an authenticated user is part of', async () => {
     const req = await request
       .agent(app)
---
+++ __tests__/githubauth.test.js
@@ -0,0 +1,48 @@
+const pool = require('../lib/utils/pool');
+const setup = require('../data/setup');
+const request = require('supertest');
+const app = require('../lib/app');
+const GithubUser = require('../lib/models/GithubUser');
+
+jest.mock('../lib/utils/github');
+
+describe('yearbook app routes', () => {
+  beforeEach(() => {
+    return setup(pool);
+  });
+
+  afterAll(() => {
+    pool.end();
+  });
+
+  it('redirects to the github oauth page upon login', async () => {
+    const req = await request(app).get('/api/v1/github/login');
+
+    expect(req.header.location).toMatch(
+      /https:\/\/github.com\/login\/oauth\/authorize\?client_id=[\w\d]+&scope=user&redirect_uri=http:\/\/localhost:7890\/api\/v1\/github\/login\/callback/i
+    );
+  });
+
+  it('should login and redirect to profile', async () => {
+    const req = await request
+      .agent(app)
+      .get('/api/v1/github/login/callback?code=42')
+      .redirects(1);
+
+    expect(req.redirects[0]).toEqual(
+      expect.stringContaining('/api/v1/profile')
+    );
+  });
+
+  it.skip('logs a user out through a delete route', async () => {
+    await GithubUser.createUser({
+      username: 'fake_github_user',
+      avatar_url: 'https://www.placecage.com/gif/300/300',
+      email: 'not-real@example.com',
+    });
+
+    const res = await request(app).delete('/api/v1/github/sessions');
+
+    expect(res.body.message).toEqual('Signed out successfully');
+  });
+});
