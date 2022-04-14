51f39ef89479946ec763632b04160a78ca7ec0f7
Hayden Indiana Holdsworth <IndianaHoldsworth@gmail.com>
2022-04-13 17:55:59 -0700

    Create profile

--- __tests__/profile.test.js
+++ __tests__/profile.test.js
@@ -15,26 +15,32 @@ describe('yearbook app routes', () => {
   });
 
   it('creates a profile', async () => {
-    const req = await request
-      .agent(app)
-      .get('/api/v1/github/login/callback?code=42')
-      .redirects(1);
+    const agent = request.agent(app);
 
-    expect(req.redirects[0]).toEqual(
-      expect.stringContaining('/api/v1/profile')
-    );
+    await agent.get('/api/v1/github/login/callback?code=42').redirects(1);
 
-    const req2 = await request.agent(app).post('/api/v1/profile').send({
-      first_name: 'Bing Bong',
-      last_name: 'Ding Dong',
+    await agent.get('/login/callback');
+
+    const req2 = await agent.post('/api/v1/profile').send({
+      avatar: 'Blue Person',
+      firstName: 'Bing Bong',
+      lastName: 'Ding Dong',
+      linkedIn: 'yeetin',
+      github: 'gitin',
       quote: 'Shablooey',
+      company: 'quan',
     });
 
+    console.log('req2.body', req2.body);
+
     expect(req2.body).toEqual({
-      id: expect.any(String),
-      first_name: expect.any(String),
-      last_name: expect.any(String),
-      quote: expect.any(String),
+      avatar: 'Blue Person',
+      firstName: 'Bing Bong',
+      lastName: 'Ding Dong',
+      linkedIn: 'yeetin',
+      github: 'gitin',
+      quote: 'Shablooey',
+      company: 'quan',
     });
   });
 });
