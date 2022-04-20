c387f3ae8f6d21c457f0c41de6c65308de02d057
Hayden Indiana Holdsworth <IndianaHoldsworth@gmail.com>
2022-04-20 09:36:30 -0700

    Get all memories route

--- __tests__/memorybook.test.js
+++ __tests__/memorybook.test.js
@@ -2,6 +2,7 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const Memorybook = require('../lib/models/Memorybook');
 
 jest.mock('../lib/utils/github');
 
@@ -37,22 +38,22 @@ describe('memorybook routes', () => {
     });
   });
 
-  it.skip('gets all items in memorybook', async () => {
+  it('gets all items in memorybook', async () => {
     const agent = request.agent(app);
     await request(app).get('/api/v1/github/login');
     // call back redirect, exchange access_token
     await agent.get('/api/v1/github/login/callback?code=42').redirects(1);
 
     // posts
-    const post1 = {
+    const post1 = await Memorybook.insert({
       id: expect.any(String),
       text: 'my first memory!',
-    };
+    });
 
-    const post2 = {
+    const post2 = await Memorybook.insert({
       id: expect.any(String),
       text: 'my second memory!',
-    };
+    });
 
     const res = await agent.get('/api/v1/memorybook');
 
