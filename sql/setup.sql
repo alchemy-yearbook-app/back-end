19123e171413e493bb1c0434bfd4da28793302cb
Kevin Do <kevind0501@gmail.com>
2022-04-22 10:57:45 -0700

    moved some seeded data

--- sql/setup.sql
+++ sql/setup.sql
@@ -81,11 +81,6 @@ INSERT INTO github_users (username, github_user_id) VALUES ('mikemike', 1);
 INSERT INTO 
     profile (avatar, first_name, last_name, linked_in, github, quote, company, user_id, email) 
 VALUES 
-
-    ('https://im3.ezgif.com/tmp/ezgif-3-64f69efea3.gif', 'Gabriel', 'Simek (TA)', 'https://www.linkedin.com/in/gabrielsimek/', 'https://github.com/gabrielsimek', 'for sureeeee', 'CareRev', '1', ''),
-
-    ('https://avatars.githubusercontent.com/u/29679939?v=4', 'Casey', 'Cameron (TA)', '', 'https://github.com/CaseyCameron', 'Get off my lawn', '', '1', ''),
-
     ('https://avatars.githubusercontent.com/u/88062154?v=4', 'Indy', 'Holdsworth', 'https://www.linkedin.com/in/h-indiana-holdsworth/', 'https://github.com/H-Indiana-Holdsworth', 'Indy out yo', '', '1', 'indianaholdsworth@gmail.com'),
 
     ('https://avatars.githubusercontent.com/u/80404797?v=4', 'Kevin', 'Do', '', 'https://github.com/kevindo1', 'lets Git this bread', '', '1', ''),
@@ -119,6 +114,10 @@ VALUES
     ('https://avatars.githubusercontent.com/u/72117040?v=4', 'Emma', 'Egstad', '', 'https://github.com/emmaegstad', 'what would karyssa do?', '', '1', ''),
 
     ('https://static.clubs.nfl.com/image/private/t_editorial_landscape_12_desktop/panthers/cxdylbkzafoaeort5joe', 'mikemike', 'jordan', 'https://www.linkedin.com/in/kdo/', 'https://github.com/kevindo1', 'i love pizza', 'piped piper', '1', 'kevind0501@gmail.com');
+
+    ('https://im3.ezgif.com/tmp/ezgif-3-64f69efea3.gif', 'Gabriel', 'Simek (TA)', 'https://www.linkedin.com/in/gabrielsimek/', 'https://github.com/gabrielsimek', 'for sureeeee', 'CareRev', '1', ''),
+
+    ('https://avatars.githubusercontent.com/u/29679939?v=4', 'Casey', 'Cameron (TA)', '', 'https://github.com/CaseyCameron', 'Get off my lawn', '', '1', ''),
     
 
 INSERT INTO cohort (github_team_id, name)
